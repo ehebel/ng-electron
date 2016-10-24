a1afa6dda09027c396ba32026b2d901f9eb200c8
ehebel <ehebel@gmail.com>
2016-10-24 20:06:06 -0300

    Refactoring de AppComponent funciona

--- build/app.js
+++ build/app.js
@@ -23394,6 +23394,8 @@ webpackJsonp([1,2],[
 	var forms_1 = __webpack_require__(611);
 	var app_component_1 = __webpack_require__(612);
 	var hero_detail_component_1 = __webpack_require__(613);
+	var heroes_component_1 = __webpack_require__(615);
+	var hero_service_1 = __webpack_require__(616);
 	var AppModule = (function () {
 	    function AppModule() {
 	    }
@@ -23405,7 +23407,11 @@ webpackJsonp([1,2],[
 	            ],
 	            declarations: [
 	                app_component_1.AppComponent,
-	                hero_detail_component_1.HeroDetailComponent
+	                hero_detail_component_1.HeroDetailComponent,
+	                heroes_component_1.HeroesComponent
+	            ],
+	            providers: [
+	                hero_service_1.HeroService
 	            ],
 	            bootstrap: [app_component_1.AppComponent]
 	        }), 
@@ -28161,30 +28167,16 @@ webpackJsonp([1,2],[
 	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 	};
 	var core_1 = __webpack_require__(350);
-	var hero_service_1 = __webpack_require__(615);
 	var AppComponent = (function () {
-	    function AppComponent(heroService) {
-	        this.heroService = heroService;
+	    function AppComponent() {
 	        this.title = 'Tour of Heroes';
 	    }
-	    AppComponent.prototype.getHeroes = function () {
-	        var _this = this;
-	        this.heroService.getHeroesSlowly().then(function (heroes) { return _this.heroes = heroes; });
-	    };
-	    AppComponent.prototype.ngOnInit = function () {
-	        this.getHeroes();
-	    };
-	    AppComponent.prototype.onSelect = function (hero) {
-	        this.selectedHero = hero;
-	    };
 	    AppComponent = __decorate([
 	        core_1.Component({
 	            selector: 'my-app',
-	            template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let hero of heroes\"\n        [class.selected]=\"hero === selectedHero\"\n        (click)=\"onSelect(hero)\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n  ",
-	            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .heroes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "],
-	            providers: [hero_service_1.HeroService]
+	            template: "\n    <h1>{{title}}</h1>\n    <my-heroes></my-heroes>\n    "
 	        }), 
-	        __metadata('design:paramtypes', [hero_service_1.HeroService])
+	        __metadata('design:paramtypes', [])
 	    ], AppComponent);
 	    return AppComponent;
 	}());
@@ -28254,7 +28246,51 @@ webpackJsonp([1,2],[
 	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 	};
 	var core_1 = __webpack_require__(350);
-	var mock_heroes_1 = __webpack_require__(616);
+	var hero_service_1 = __webpack_require__(616);
+	var HeroesComponent = (function () {
+	    function HeroesComponent(heroService) {
+	        this.heroService = heroService;
+	    }
+	    HeroesComponent.prototype.getHeroes = function () {
+	        var _this = this;
+	        this.heroService.getHeroesSlowly().then(function (heroes) { return _this.heroes = heroes; });
+	    };
+	    HeroesComponent.prototype.ngOnInit = function () {
+	        this.getHeroes();
+	    };
+	    HeroesComponent.prototype.onSelect = function (hero) {
+	        this.selectedHero = hero;
+	    };
+	    HeroesComponent = __decorate([
+	        core_1.Component({
+	            selector: 'my-heroes',
+	            template: "\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let hero of heroes\"\n        [class.selected]=\"hero === selectedHero\"\n        (click)=\"onSelect(hero)\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n  ",
+	            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .heroes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "],
+	            providers: [hero_service_1.HeroService]
+	        }), 
+	        __metadata('design:paramtypes', [hero_service_1.HeroService])
+	    ], HeroesComponent);
+	    return HeroesComponent;
+	}());
+	exports.HeroesComponent = HeroesComponent;
+
+
+/***/ },
+/* 616 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(350);
+	var mock_heroes_1 = __webpack_require__(617);
 	var HeroService = (function () {
 	    function HeroService() {
 	    }
@@ -28278,7 +28314,7 @@ webpackJsonp([1,2],[
 
 
 /***/ },
-/* 616 */
+/* 617 */
 /***/ function(module, exports) {
 
 	"use strict";
